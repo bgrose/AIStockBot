83a8311752a224a9400da3cbd9fa7549a2c354bc
bgrose <bradley@grose.net>
2020-05-24 15:34:31 -0400

    Creates Frame for Key Storage

--- AlpacaKey.py
+++
@@ -1,7 +0,0 @@
-class AlpacaKey(REST):
-    def __int__(self):
-        super().__init__(
-            key_id='KEY',
-            secret_key='KEY',
-            base_url='https://paper-api.alpaca.markets'
-        )
